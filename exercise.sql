68a12cbb783bec23a18b22c85466c4a8da9d2f35
Captaincrogs <54841728+Captaincrogs@users.noreply.github.com>
2020-01-10 10:31:12 +0100

    Update exercise.sql

--- exercise.sql
+++ exercise.sql
@@ -1,1 +1,7 @@
-INSERT INTO `sterrenstelsel`.`planeet` (`naam`) VALUES ('');
+//INSERT INTO `sterrenstelsel`.`planeet` (`naam`) VALUES ('');
+
+INSERT INTO `sterrenstelsel`.`planeet` (`naam`) VALUES ('zon');
+INSERT INTO `sterrenstelsel`.`planeet` (`naam`) VALUES ('Mercurius');
+INSERT INTO `sterrenstelsel`.`planeet` (`naam`) VALUES ('Venus');
+INSERT INTO `sterrenstelsel`.`planeet` (`naam`) VALUES ('Aarde');
+INSERT INTO `sterrenstelsel`.`planeet` (`naam`) VALUES ('Mars');
